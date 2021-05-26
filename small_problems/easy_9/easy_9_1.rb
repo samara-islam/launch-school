ff9ad9de13b8bf7bbd7fa0302befff31407e4f27
Samara Islam <sz.islam@outlook.com>
2021-05-25 17:29:31 -0700

    easy 9-1

---
+++ small_problems/easy_9/easy_9_1.rb
@@ -0,0 +1,5 @@
+def greetings(arr, hsh)
+  puts "Hello, #{arr.join(' ')}! Nice to have a #{hsh[:title]} #{hsh[:occupation]} around."
+end
+
+greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
